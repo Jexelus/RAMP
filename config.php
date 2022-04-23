3ecc5d9819cf84c30c0d27b0d63b43964444810b
Jexelus <103692028+Jexelus@users.noreply.github.com>
2022-04-23 13:45:13 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+define('DBSERVER', 'localhost'); // Database server
+define('DBUSERNAME', 'root'); // Database username
+define('DBPASSWORD', ''); // Database password
+define('DBNAME', 'regramp'); // Database name
+file_put_contents('D:\logs.txt', print_r('123', true));
+/* connect to MySQL database */
+$db = mysqli_connect(DBSERVER, DBUSERNAME, DBPASSWORD, DBNAME);
+ 
+file_put_contents('D:\logs.txt', print_r($db, true));
+if($db === false){
+    die("Error: connection error. " . mysqli_connect_error());
+}
+?>
